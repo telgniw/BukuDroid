b6bd02b7b58f668e769198212ff04423e77a0541
Yi Huang <b97902044@ntu.edu.tw>
2011-11-26 17:21:39 +0800

    ADD create actionbar

--- Mobile/BukuDroid/src/org/csie/mpp/buku/MainActivity.java
+++ Mobile/BukuDroid/src/org/csie/mpp/buku/MainActivity.java
@@ -2,10 +2,16 @@ package org.csie.mpp.buku;
 
 import android.os.Bundle;
 
+import com.markupartist.android.widget.ActionBar;
+
 public class MainActivity extends BaseActivity {
+	protected ActionBar actionbar;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        actionbar = (ActionBar)findViewById(R.id.actionbar);
     }
 }
