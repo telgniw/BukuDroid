0aeaac794cd37c16223f1cd563c7aca9781ca57b
ianchou <ianchou821@gmail.com>
2011-12-11 15:24:41 +0800

    redirect the user to Android market if barcode scanner didn't be installed

--- Mobile/BukuDroid/src/org/csie/mpp/buku/ScanActivity.java
+++ Mobile/BukuDroid/src/org/csie/mpp/buku/ScanActivity.java
@@ -1,8 +1,17 @@
 package org.csie.mpp.buku;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,16 +24,16 @@ public class ScanActivity extends Activity implements OnClickListener {
 	protected EditText isbn;
 	protected Button barcode;
 	protected Button add;
-	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan);
-        
+
         isbn = (EditText)findViewById(R.id.isbn);
         barcode = (Button)findViewById(R.id.barcode);
         add = (Button)findViewById(R.id.add);
-        
+
         barcode.setOnClickListener(new OnClickListener(){
 
 			@Override
@@ -32,6 +41,11 @@ public class ScanActivity extends Activity implements OnClickListener {
 				//TODO(ianchou): integrate bar code scanning
 		        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
 		        intent.putExtra("SCAN_MODE", "ONE_D_MODE");
+		        String targetAppPackage = findTargetAppPackage(intent);
+		        if (targetAppPackage == null) {
+		          showDownloadDialog();
+		          return;
+		        }
 		        startActivityForResult(intent, 0);
 			}
         	
@@ -61,4 +75,42 @@ public class ScanActivity extends Activity implements OnClickListener {
 		setResult(RESULT_FIRST_USER, data);
 		finish();
 	}
+
+	private String findTargetAppPackage(Intent intent) {
+	   PackageManager pm = ScanActivity.this.getPackageManager();
+	   List<ResolveInfo> availableApps = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+	   if (availableApps!=null && !availableApps.isEmpty()) {
+		   return availableApps.get(0).activityInfo.packageName;
+	   }
+	   return null;
+	}
+
+	private AlertDialog showDownloadDialog() {
+		AlertDialog.Builder downloadDialog = new AlertDialog.Builder(ScanActivity.this);
+		downloadDialog.setTitle("Install Barcode Scanner");
+		downloadDialog.setMessage("This funcion requires Barcode Scanner. Would you like to install it?");
+		downloadDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+				Uri uri = Uri.parse("market://details?id=com.google.zxing.client.android");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				try {
+					ScanActivity.this.startActivity(intent);
+				} catch (ActivityNotFoundException anfe) {
+					// Hmm, market is not installed
+					Log.w(ScanActivity.class.getSimpleName(), "Android Market is not installed; cannot install Barcode Scanner");
+				}
+			}
+
+		});
+		downloadDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {}
+
+		});
+		return downloadDialog.show();
+	}
+
 }
