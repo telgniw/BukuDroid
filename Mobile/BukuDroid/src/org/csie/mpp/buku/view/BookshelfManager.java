21bcd7230cdb87365eabdd373bd44d87b1841571
Yi Huang <b97902044@ntu.edu.tw>
2011-12-13 22:41:35 +0800

    UPDATE bookshelf listview

--- Mobile/BukuDroid/src/org/csie/mpp/buku/view/BookshelfManager.java
+++ Mobile/BukuDroid/src/org/csie/mpp/buku/view/BookshelfManager.java
@@ -1,5 +1,10 @@
 package org.csie.mpp.buku.view;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.csie.mpp.buku.App;
 import org.csie.mpp.buku.R;
 import org.csie.mpp.buku.ScanActivity;
@@ -10,17 +15,81 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class BookshelfManager extends ViewManager {
-	protected ListView booklist;
-	protected ArrayAdapter<String> booklistAdapter;
+	private interface ViewManager {
+		public void initView(View view);
+		public int length();
+		public void setBooks(BookEntry[] entries);
+		public void addBook(BookEntry entry);
+		public void remove(BookEntry entry);
+	};
+	
+	private class ListViewManager implements ViewManager {
+		private static final String FIELD_ICON = "ICON", FIELD_TITLE = "TITLE", FIELD_AUTHOR = "AUTHOR";
+		
+		private BookEntry[] entries; 
+		private ListView booklist;
+		private SimpleAdapter booklistAdapter;
+		private List<Map<String, Object>> list_items;
+		
+		@Override
+		public void initView(View view) {
+			booklist = (ListView)view.findViewById(R.id.inner_list);
+			list_items = new ArrayList<Map<String, Object>>();
+			booklistAdapter = new SimpleAdapter(
+				activity, list_items, R.layout.list_item_book, new String[] {
+					FIELD_ICON, FIELD_TITLE, FIELD_AUTHOR
+				}, new int[] {
+					R.id.list_image, R.id.list_title, R.id.list_author
+				}
+			);
+			booklist.setAdapter(booklistAdapter);
+		}
+		
+		@Override
+		public int length() {
+			return entries == null? 0 : entries.length;
+		}
+		
+		@Override
+		public void setBooks(BookEntry[] es) {
+			entries = es;
+			for(BookEntry entry: entries)
+				_addBook_(entry);
+			booklistAdapter.notifyDataSetChanged();
+		}
+		
+		private void _addBook_(BookEntry entry) {
+			Map<String, Object> item = new HashMap<String, Object>();
+			item.put(FIELD_ICON, R.drawable.book);
+			item.put(FIELD_TITLE, entry.isbn);
+			item.put(FIELD_AUTHOR, "author");
+			list_items.add(item);
+		}
+
+		@Override
+		public void addBook(BookEntry entry) {
+			_addBook_(entry);
+			booklistAdapter.notifyDataSetChanged();
+		}
+
+		@Override
+		public void remove(BookEntry entry) {
+			
+		}
+	};
+	
+	protected ListViewManager vm;
 	
 	public BookshelfManager(Activity activity, DBHelper helper) {
 		super(activity, helper);
+		
+		vm = new ListViewManager();
 	}
 	
 	public void onResultCallback(int requestCode, int resultCode, Intent data) {
@@ -30,20 +99,18 @@ public class BookshelfManager extends ViewManager {
 				entry.isbn = data.getStringExtra(ScanActivity.ISBN);
 				if(!entry.insert(wdb))
 					Log.e(App.TAG, "Insert failed \"" + entry.isbn + "\".");
-				updateBooklist();
+				updateBooklist(entry);
 			}
 		}
 	}
 	
 	private void updateBooklist() {
-		if(booklist == null)
-			updateView();
-		else {
-			BookEntry[] entries = BookEntry.queryAll(rdb);
-			for(BookEntry entry: entries)
-				booklistAdapter.add(entry.isbn);
-					booklistAdapter.notifyDataSetChanged();
-		}
+		BookEntry[] entries = BookEntry.queryAll(rdb);
+		vm.setBooks(entries);
+	}
+	
+	private void updateBooklist(BookEntry entry) {
+		vm.addBook(entry);
 	}
 
 	@Override
@@ -65,9 +132,7 @@ public class BookshelfManager extends ViewManager {
 		else {
 			View view = activity.getLayoutInflater().inflate(R.layout.list, null);
 			frame.addView(view);
-			booklist = (ListView)view.findViewById(R.id.inner_list);
-			booklistAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1);
-			booklist.setAdapter(booklistAdapter);
+			vm.initView(view);
 			updateBooklist();
 		}
 	}
